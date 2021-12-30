2b2896e92bb496e6fffac71b0c154d1b57dd0890
vishalkumar215 <vishalchoubey2@gmail.com>
2021-12-30 23:24:06 +0530

    added

---
+++ recurssion in arraylist/getsequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.ArrayList;
+public class getsequence{
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.next();
+        ArrayList<String> ans = gss(s);
+        System.out.println(ans);
+    }
+
+    public static ArrayList<String> gss(String str) {
+        if(str.length() == 0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+            
+        }
+        
+        
+        char ch = str.charAt(0);        //a
+        String ros = str.substring(1);      //bc
+        
+        ArrayList<String> recAns = gss(ros);
+        ArrayList<String> myAns = new ArrayList<>(); 
+        
+       
+        for(String ele : recAns){
+            myAns.add(ele);
+        }
+        
+         for(String ele : recAns){
+            myAns.add(ch+ele);
+        }
+        
+        return myAns;
+        
+    }
+
+}
+
+
+
+
+
+
+    
+    
